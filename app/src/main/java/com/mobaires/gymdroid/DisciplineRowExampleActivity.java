d81d6048b64b4e78935e601a88c3335a31ef4eb6
Gabriel Calero <g@mobaires.com>
2016-01-14 22:13:10 -0300

    Adding TODOs for ToolBar

--- app/src/main/java/com/mobaires/gymdroid/DisciplineRowExampleActivity.java
+++ app/src/main/java/com/mobaires/gymdroid/DisciplineRowExampleActivity.java
@@ -10,4 +10,12 @@ public class DisciplineRowExampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discipline_row_example);
     }
+
+    // TODO - include the toolbar in this activity too
+    // TODO - the toolbar must contain the share button
+
+    // TODO - incluir la Toolbar también en este activity
+    // TODO - la Toolbar deber contener el botón de Share
+
+
 }
--- app/src/main/java/com/mobaires/gymdroid/MainActivity.java
+++ app/src/main/java/com/mobaires/gymdroid/MainActivity.java
@@ -1,7 +1,8 @@
 package com.mobaires.gymdroid;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
 
 import com.mobaires.gymdroid.data.Discipline;
 
@@ -12,6 +13,9 @@ public class MainActivity extends FragmentActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
     }
 
     @Override
@@ -19,5 +23,22 @@ public class MainActivity extends FragmentActivity
         // TODO - if a discipline is selected, we should do something with it (show it maybe?)
         /* TODO - Si una disciplina se selecciona, debemos hacer algo con ella, como mostrarla en
                   otro fragment o abrir un nuevo activity.. */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(
+                R.menu.menu_main, menu);
+
+        // TODO - 1. Setup the search bar to filter disciplines by name
+        // TODO - 2. Setup the share provider to share the data of the selected discipline
+        // TODO - 3. The share button should be disabled until a discipline is selected
+        // TODO - 4.
+
+        // TODO - 1. Configurar la barra de búsqueda para que filtre disciplinas por nombre
+        // TODO - 2. Configurar el Share Provider para que comparta los datos de la disciplina seleccionada
+        // TODO - 3. El botón de compartir debería estar deshabilitado mientras no hay una disciplina selecionada
+
+        return true;
     }
 }
